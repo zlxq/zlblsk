827ed778bddce4005863a158df31c1deef4632d9
zlxq <ta_000zhanglei@126.com>
2019-05-20 01:36:15 +0800

    修改rbc工具类

--- zlxq/zlxq-rbac/src/main/java/com/zlxq/rbac/base/util/ConstantRBAC.java
+++ zlxq/zlxq-rbac/src/main/java/com/zlxq/rbac/base/util/ConstantRBAC.java
@@ -1,5 +1,7 @@
 package com.zlxq.rbac.base.util;
 
+import com.framework.util.Constant;
+
 /**
  * @ClassName: ConstantRBAC.java
  * @Description: TODO(方法描述)
@@ -10,9 +12,17 @@ package com.zlxq.rbac.base.util;
  * @Copyright: 2019 www.zlxq.com Inc. All rights reserved.
  *
  */
-public class ConstantRBAC {
+public class ConstantRBAC extends Constant {
 
+	public static final String IS_SUPER = "1";
+	public static final String Y_ISVALIDATE = "1";
+	public static final String N_ISVALIDATE = "0";
+	
 	public static final String SUCCESS = "success";
 	public static final String FAILURE = "failure";
 	public static final String REDIECT = "rediect";
+	
+	public static final String IS_LEAF_O = "open";
+	public static final String IS_LEAF_C = "close";
+	public static final String ICONCLS_DEFAULT = "icon-city";
 }
--- zlxq/zlxq-rbac/src/main/java/com/zlxq/rbac/base/util/DictUtil.java
+++ zlxq/zlxq-rbac/src/main/java/com/zlxq/rbac/base/util/DictUtil.java
@@ -1,21 +1,28 @@
 package com.zlxq.rbac.base.util;
 
-/**
- * @ClassName: DictUtil.java
- * @Description: TODO(方法描述)
- * @author: PUB
- * @date: 2019年4月17日 下午10:30:24
- *
- * @param <T>
- * @Copyright: 2019 www.zlxq.com Inc. All rights reserved.
- *
- */
 public class DictUtil {
 
-	public static String roleString = "100";
+	public static String roleString = "100"; //角色
+	public static String deptString = "200"; //party表中partytype类型
+	public static String partyRelationString = "200"; //partyrelation表中type类型
+	
 	
 	public static final class role_lx {
-		public static String role_lx_super = "100100";
-		public static String role_lx_yb = "100100";
+		public static String role_super = "100100";//超级管理员
+		public static String role_xtgly = "100200";//系统管理员
+	}
+	
+	public static final class dept_lx {
+		public static String dept_gs = "200100";//公司
+		public static String dept_bm = "200200";//部门
+		public static String dept_ry = "200300";//人员
+	}
+	
+	public static final class partyRel_lx {
+		public static String partyRel_gs = "300100";//公司
+		public static String partyRel_gshzz = "300200";//组织和组织
+		public static String partyRel_zzhyhz = "300300";//组织和用户组
+		public static String partyRel_zzhr = "300400";//组织和人
+		public static String partyRel_yhzhr = "300500";//用户组和人
 	}
 }
