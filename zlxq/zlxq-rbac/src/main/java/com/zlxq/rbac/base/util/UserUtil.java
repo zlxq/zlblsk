fe2b783abf909c2c0b9a294f02c43cadbda5173b
zlxq <ta_000zhanglei@126.com>
2019-05-20 01:37:59 +0800

    增加获取当前登录用户类

---
+++ zlxq/zlxq-rbac/src/main/java/com/zlxq/rbac/base/util/UserUtil.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.zlxq.rbac.base.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import pojo.ZlxqParty;
+
+/**
+ * @TODO 获取登录用户信息
+ * @author zhangl
+ *
+ * @createtime 2019年5月19日
+ * @version V1.0
+ */
+public class UserUtil {
+
+	public static ZlxqParty getZlxqParty() {
+		HttpServletRequest request = ServletActionContext.getRequest();
+		return getPubSysuser(request.getSession());
+	}
+
+	public static ZlxqParty getPubSysuser(HttpSession hs) {
+		ZlxqParty pa = (ZlxqParty) hs.getAttribute("user");
+		return pa;
+	}
+	
+	public static Long getCompanyId() {
+		return getZlxqParty().getCompanyId();
+	}
+	
+	public static Long getCurDeptId() {
+		return getZlxqParty().getCruDeptId();
+	}
+	
+
+	public static Long getUserId() {
+		return getZlxqParty().getId();
+	}
+	
+	public static String getPartyno() {
+		return getZlxqParty().getPartyno();
+	}
+	
+	public static String getPartyName() {
+		return getZlxqParty().getPartname();
+	}
+}
